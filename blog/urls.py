ef2b6f3c7fee10991a1fa2d7c8933e0376a26665
alvaro.paricio <alvaro.paricio@coit.es>
2016-09-06 17:12:43 +0200

    1.0.3

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from . import views
 
+app_name="blog"
 urlpatterns = [
-	url(r'^$', views.post_list),
+  url(r'^$', views.v_post_list),
+  url(r'^post/(?P<param_pk>[0-9]+)/$', views.v_post_detail, name='v_post_detail' ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
-def post_list(request):
+def v_post_list(request):
   # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
   posts = Post.objects.filter()
   return render(request, 'blog/post_list.html', {'posts': posts} )
 
-# Create your views here.
+def v_post_detail(request, param_pk="1"):
+  post = get_object_or_404(Post, pk=param_pk)
+  return render(request, 'blog/post_detail.html', {'post': post})
+
